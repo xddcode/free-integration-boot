536b0d5b3dbc27e833fa67d41069d2bedf925ddb
freedom_dh <18202969762@163.com>
2021-03-31 13:58:07 +0800

    新增常量

---
+++ free-common/src/main/java/com/free/common/constant/EsIndexConstant.java
@@ -0,0 +1,12 @@
+package com.free.common.constant;
+
+/**
+ * ES索引常量
+ *
+ * @author dinghao
+ * @date 2021/3/31
+ */
+public interface EsIndexConstant {
+
+    String ARTICLE_INDEX = "ik_article";
+}
